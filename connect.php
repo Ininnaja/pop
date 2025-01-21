0a14efeb60f842e626d0b2adfe2640080223a4d0
Ininnaja <inthirajail94@gmail.com>
2025-01-21 23:22:44 +0700

    connect database

--- connect.php
+++ connect.php
@@ -1,13 +1,13 @@
 <?php
 
-    $servername = 'localhost';
+    $servername = 'localhost:3307';
     $username = 'root';
     $password = '';
     $dbname = 'pop';
 
     try {
 
-        $conn = new PDO("mysql:host = $servername; dbname=$dbname", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
     } catch(PDOException $e){
